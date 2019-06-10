dc55307f5ca052bca271d40e7cef5ab95066a750
midudev <miduga@gmail.com>
2019-06-10 14:38:53 -0500

    Mostrando información real en el ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
-        [1, 2, 3, 4].map(category => <Item key={category}><Category /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
