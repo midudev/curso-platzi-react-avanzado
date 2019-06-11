9407b974e4c555d7681eb6738a53d9f2aeb7db8c
midudev <miduga@gmail.com>
2019-06-11 09:05:14 -0500

    Añadir animaciones keyframes y reusarlas

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const ImgWrapper = styled.div`
   border-radius: 10px;
@@ -11,6 +12,7 @@ export const ImgWrapper = styled.div`
 `
 
 export const Img = styled.img`
+  ${fadeIn()}
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
   height: 100%;
   object-fit: cover;
---
+++ src/styles/animation.js
@@ -0,0 +1,16 @@
+import { keyframes, css } from 'styled-components'
+
+const fadeInKeyframes = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0;
+  }
+
+  to {
+    filter: blur(0);
+    opacity: 1;
+  }
+`
+
+export const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
+  css`animation: ${time} ${fadeInKeyframes} ${type};`
