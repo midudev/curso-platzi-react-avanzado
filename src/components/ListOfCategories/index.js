5515c6b7fe3286e1d8675af356aa17c979590c4b
midudev <miduga@gmail.com>
2019-06-11 10:14:04 -0500

    useEffect, limpiando eventos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     window.fetch('https://petgram-server.midudev.now.sh/categories')
@@ -14,11 +15,29 @@ export const ListOfCategories = () => {
       })
   }, [])
 
-  return (
-    <List>
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,6 +4,20 @@ export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  &.fixed {
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(.5);
+    z-index: 1;
+  }
 `
 
 export const Item = styled.li`
