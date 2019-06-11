92f1fb481b95e1bc851812077fd2f358f5ff563c
midudev <miduga@gmail.com>
2019-06-11 09:51:17 -0500

    Añadiendo useState y useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://petgram-server.midudev.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
