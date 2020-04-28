a719c9ce1c5ade03ec9747aec2e9e76a2bc6f3c8
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-28 13:23:21 -0500

    Information for Catergories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
+import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
   return (
     <List>
-      {[1, 2, 3, 4].map((category) => (
-        <Item key={category}>
-          <Category />
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
         </Item>
       ))}
     </List>
