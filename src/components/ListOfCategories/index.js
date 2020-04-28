32bd36b4241dbe38b898e73fd14505c9f2f4dd00
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-28 16:01:53 -0500

    Adding Hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await window.fetch('https://the-petgram-server.now.sh/categories')
+      const data = await response.json()
+      setCategories(data)
+    }
+    fetchCategories()
+  }, [])
   return (
     <List>
       {categories.map((category) => (
