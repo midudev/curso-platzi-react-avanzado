560b4b5775b0ab0c773d1b484fc6810242b704ef
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-28 14:50:43 -0500

    Svg to component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
