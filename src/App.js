a757590f04ad0db2fcd6eed31608fedef0b9e64a
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-28 15:14:38 -0500

    Animations with keyframes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 
---
+++ src/styles/animation.js
@@ -0,0 +1,17 @@
+import { css, keyframes } from 'styled-components'
+
+const fadeInKeyframes = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0
+  }
+  to {
+    filter: blur(0);
+    opacity: 1
+  }
+`
+
+export const fadeIn = ({ time = '1s', type = 'ease' } = {}) =>
+  css`
+    animation: ${time} ${fadeInKeyframes} ${type};
+  `
