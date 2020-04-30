d30758f9205336d20a2cec62806c631242687363
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-30 09:55:07 -0500

    Params for query

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,23 +1,7 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
-
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   console.log(photos)
 
   return (
@@ -28,5 +12,3 @@ const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
---
+++ src/hoc/withPhotos.js
@@ -0,0 +1,15 @@
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+export const withPhotos = graphql(gql`
+  query getPhotos($categoryId: ID) {
+    photos(categoryId: $categoryId) {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`)
