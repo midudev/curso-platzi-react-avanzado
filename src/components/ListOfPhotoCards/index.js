8d897adf8a843e0e4c7a9daee8b123bdeace715e
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-04-30 09:37:29 -0500

    React Apollo Client

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+const withPhotos = graphql(gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+  console.log(photos)
+
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6].map((id) => (
-        <PhotoCard key={id} id={id} />
+      {photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
       ))}
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const cache = new InMemoryCache()
+
+const link = new HttpLink({
+  uri: 'https://the-petgram-server.now.sh/graphql'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
