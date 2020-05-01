c255518277da98d74aa21c6f3cd065b51d24558d
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-05-01 11:12:34 -0500

    Detail page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {detailId ? (
-        <PhotoCardWithQuery detailId={detailId} />
-      ) : (
-        <Router>
-          <Home path='/' />
-          <Home path='/pet/:id' />
-        </Router>
-      )}
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </>
   )
 }
