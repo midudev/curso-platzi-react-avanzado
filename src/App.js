69c6053eb4ad26206f3078f05006b7c17c976cf1
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-05-01 13:04:17 -0500

    Protected routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React from 'react'
 import { Layout } from './components/Layout'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -14,6 +21,22 @@ export const App = () => {
           <Home path='/pet/:id' />
           <Detail path='/detail/:detailId' />
         </Router>
+
+        <UserLogged>
+          {({ isAuth }) =>
+            isAuth ? (
+              <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+            ) : (
+              <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+            )
+          }
+        </UserLogged>
       </Layout>
     </>
   )
---
+++ src/pages/Favs.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Favs = () => {
+  return <div>Esto es favs</div>
+}
