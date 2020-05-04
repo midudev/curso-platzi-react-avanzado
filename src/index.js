7ede962fc8fe200fbcec524bf78844d093b08574
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-05-04 11:34:30 -0500

    Like as a registered user

--- src/index.js
+++ src/index.js
@@ -14,7 +14,23 @@ const link = new HttpLink({
 
 const client = new ApolloClient({
   cache,
-  link
+  link,
+  request: (operation) => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: (error) => {
+    const { networkError } = error
+    if (networkError && networkError.result.code === 'invalid_token') {
+      window.sessionStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 ReactDOM.render(
