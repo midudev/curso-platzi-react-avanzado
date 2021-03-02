34f2a7018ea87415fc10de96cfeb1c5cb574a958
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-02 11:51:50 -0500

    Vecerl deploy, Linter and extensions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Regards</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Regards</h1>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    output: {
-        filename: 'app.bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js|jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    }
-}
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
