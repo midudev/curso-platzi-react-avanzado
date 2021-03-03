fe9e57450a714473de54b8dc3640ca7b12d2d04b
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-03 16:26:01 -0500

    Using IntersectionObserver

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { PhotoCard } from '../PhotoCard';
 
-import {List} from './styles';
+import { List } from './styles';
 export const ListOfPhotoCards = () => {
     return (
         <List>
             {
                 [1, 2, 3, 4].map(id => (
-                    <li key={id}>
-                        <PhotoCard />
-                    </li>
+                    <PhotoCard key={id} />
                 ))
             }
         </List>
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { MdFavoriteBorder } from "react-icons/md";
-import {ImgWrapper, Img, Button} from './styles';
+import { Article, ImgWrapper, Img, Button } from './styles';
 
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png"
-export const PhotoCard = ({id, likes = 0, src=DEFAULT_IMAGE}) =>{
-    return(
-        <article>
-            <a href={`/details/${id}`}>
-                <ImgWrapper>
-                    <Img src={src}/>
-                </ImgWrapper>
-            </a>
-            <Button><div><MdFavoriteBorder size="20px" color="#00bdff"/></div> {likes} Likes!</Button>
-        </article>
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+    const refElement = useRef(null);
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        const observer = new window.IntersectionObserver((entries) => {
+            const { isIntersecting } = entries[0];
+            console.log(isIntersecting);
+            if (isIntersecting) {
+                setShow(true)
+                observer.disconnect();
+            }
+        })
+        observer.observe(refElement.current);
+    }, [refElement])
+
+    return (
+        <Article ref={refElement}>
+            {show &&
+                <Fragment>
+                    <a href={`/details/${id}`}>
+                        <ImgWrapper>
+                            <Img src={src} />
+                        </ImgWrapper>
+                    </a>
+                    <Button><div><MdFavoriteBorder size="20px" color="#00bdff" /></div> {likes} Likes!</Button>
+                </Fragment>
+            }
+        </Article>
     )
 }
