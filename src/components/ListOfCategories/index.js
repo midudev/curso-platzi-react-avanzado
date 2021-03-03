a070f946a98c5294f927c0c9958f83a8ef249372
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-03 10:49:08 -0500

    Adding Hooks to ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Category } from '../Category/index';
-import { categories } from '../../../api/db.json';
+// import { categories as mockCategories } from '../../../api/db.json';
 
 import { List, Item, Rapper } from './styles';
 export const ListOfCategories = () => {
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+        const fetchCategories = async () =>{
+            try {
+                const response = await window.fetch('https://petgram-server-edsf8xpy2.now.sh/categories');
+                if (!response.ok){
+                    throw new Error('HTTP Status ' + response.status)
+                }
+                const data = await response.json();
+
+                setCategories(data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        }
+        fetchCategories();
+    }, []);
     return (
         <Rapper>
             <List>
