b51e757840a7f6d42c337ee1eff72fcae25f4708
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-05 15:51:00 -0500

    Improving Logo styles

--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
-    padding: 20px
+    align-items: center;
+    padding: 10px 8px;
+    border-bottom: 1px solid #e0e0e0;
 `
 export const Svg = styled.svg`
-    width: 220px;
-    /* margin-top: -50px */
+    width: 150px;
 `
