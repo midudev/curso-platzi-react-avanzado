b20650fd8ab3f3c07ba9adeb2fe3fc62ddd3f4a6
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-05 11:40:06 -0500

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { Home } from './pages/Home'
 import { Logo } from './components/Logo';
 import { Navbar } from './components/Navbar';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
 
 
-
+const UserLogged = ({ children }) => {
+    return children({ isAuth: false })
+}
 import { GlobalStyle } from './styles/GlobalStyles';
 export const App = () => {
     return (
@@ -17,7 +22,25 @@ export const App = () => {
                 <Detail path='/detail/:detailId' />
                 <Home path='/' />
                 <Home path='/pet/:id' />
+
             </Router>
+            <UserLogged>
+                {
+                    ({ isAuth }) =>
+                        isAuth
+                            ?
+                            <Router>
+                                <Favs path='/favs' />
+                                <User path='/user' />
+                            </Router>
+                            :
+                            <Router>
+                                <NotRegisteredUser path='/favs' />
+                                <NotRegisteredUser path='/user' />
+                            </Router>
+                }
+
+            </UserLogged>
             <Navbar />
 
         </Fragment>
