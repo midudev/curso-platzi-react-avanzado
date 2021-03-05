515ef044e49ad00ba5fc2bca6b9235284849814a
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-05 09:26:44 -0500

    Link component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Link } from "@reach/router";
-import { Anchor, Image } from './styles';
+import { Link, Image } from './styles';
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg';
 
-export const Category = ({ cover = DEFAULT_IMAGE, id, emoji = "?" }) => {
+export const Category = ({ cover = DEFAULT_IMAGE, path = '#', emoji = "?" }) => {
     return (
-        <Anchor href={`/pet/${id}`}>
+        <Link to={path}>
             <Image src={cover} alt="Category" />
             {emoji}
-        </Anchor>
+        </Link>
     )
 }
--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-
-export const Anchor = styled.a`
+import { Link as LinkRouter } from "@reach/router";
+export const Link = styled(LinkRouter)`
     display:flex;
     flex-direction: column;
     text-align: center;
