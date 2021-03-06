ce49e423b32f3b44c99659696f5d6245272a5287
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-05 22:13:56 -0500

    RegisterMutation component and register auth

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -5,12 +5,16 @@ import { Form, Input, Button, Title, Text } from './styles';
 export const UserForm = ({ onSubmit, title, text }) => {
     const email = useInputValue('')
     const password = useInputValue('')
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        onSubmit({email : email.value,password: password.value})
+    }
     return (
         <Fragment>
             <Fox/>
             <Title>{title}</Title>
             <Text>{text}</Text>
-            <Form onSubmit={onSubmit}>
+            <Form onSubmit={handleSubmit}>
                 <Input placeholder="Email" {...email} />
                 <Input type="password" placeholder="password" {...password} />
                 <Button>{title}</Button>
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import Context from '../Context';
-
+import { RegisterMutation } from '../containers/RegisterMutation';
 import { UserForm } from '../components/UserForm';
 export const NotRegisteredUser = () => (
     <Context.Consumer>
@@ -8,7 +8,18 @@ export const NotRegisteredUser = () => (
             ({ activateAuth }) => {
                 return <Fragment>
                     <UserForm onSubmit={activateAuth} title={'Log in'} text={"Log in to see photos and videos of pets."} />
-                    <UserForm onSubmit={activateAuth} title={'Sign Up'} text={"Sign up to see photos and videos of pets."} />
+                    <RegisterMutation>
+                        {
+                            (register) => {
+                                const onSubmit = ({ email, password }) => {
+                                    const input = { email, password };
+                                    const variables = { input };
+                                    register({ variables }).then(activateAuth)
+                                }
+                                return <UserForm onSubmit={onSubmit} title={'Sign Up'} text={"Sign up to see photos and videos of pets."} />
+                            }
+                        }
+                    </RegisterMutation>
                 </Fragment>
             }
         }
