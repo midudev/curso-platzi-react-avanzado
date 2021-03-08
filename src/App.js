ac772b3336f95a5a4bc36c3d2cbb6f08919b217c
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-08 12:03:08 -0500

    NotFound page fixed error

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ export const App = () => {
                 <Home path='/' />
                 <Home path='/pet/:id' />
                 {!isAuth && <NotRegisteredUser path='/login' />}
-                {!isAuth && <Redirect from='/favs' to='/login' />}
-                {!isAuth && <Redirect from='/user' to='/login' />}
-                {isAuth && <Redirect from='/login' to='/' />}
+                {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+                {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+                {isAuth && <Redirect noThrow from='/login' to='/' />}
                 <Favs path='favs' />
                 <User path='user' />
             </Router>
