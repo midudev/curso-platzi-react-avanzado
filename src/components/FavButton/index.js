65853b86d0157a0de2c47ea92d15603663c9e156
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-09 08:41:59 -0500

    Login to Like

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import { Context } from '../../Context';
 import { Likes } from './styles';
 import PropTypes from 'prop-types';
+import { ErrorMessage } from '../ErrorMessage';
 import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 export const FavButton = ({ likes, liked, onClick }) => {
     const Icon = liked ? MdFavorite : MdFavoriteBorder;
+    const { isAuth } = useContext(Context);
+    console.log(isAuth);
     return (
-        <Likes><button onClick={onClick}><Icon size="20px" color="#00bdff" /></button> {likes} Likes!</Likes>
+        <Fragment>
+            <Likes><button onClick={onClick}><Icon size="20px" color="#00bdff" /></button> {likes} Likes!</Likes>
+            {!isAuth && < ErrorMessage message='You must be log in to like the photo' />}
+        </Fragment>
     )
 }
 
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,15 +1,14 @@
-import React, { Fragment} from 'react';
-import {Link} from '@reach/router';
+import React, { Fragment } from 'react';
+import { Link } from '@reach/router';
 import PropTypes from 'prop-types';
 import { useNearScreen } from '../../hooks/useNearScreen';
 import { FavButton } from '../FavButton';
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation';
-
 import { Article, ImgWrapper, Img } from './styles';
 
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
-export const PhotoCard = ({ id,liked, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
 
     const [show, refElement] = useNearScreen();
 
@@ -26,9 +25,11 @@ export const PhotoCard = ({ id,liked, likes = 0, src = DEFAULT_IMAGE }) => {
                         {
                             (toggleLike) => {
                                 const handleFavClick = () => {
-                                    toggleLike({variables: {
-                                        input: {id}
-                                    }})
+                                    toggleLike({
+                                        variables: {
+                                            input: { id }
+                                        }
+                                    })
                                 }
                                 return <FavButton likes={likes} liked={liked} onClick={handleFavClick} />
                             }
@@ -43,13 +44,13 @@ PhotoCard.propTypes = {
     id: PropTypes.string.isRequired,
     liked: PropTypes.bool.isRequired,
     src: PropTypes.string.isRequired,
-    likes: function(props, propName, componentName){
+    likes: function (props, propName, componentName) {
         const propValue = props[propName];
-        if(propValue === undefined){
+        if (propValue === undefined) {
             return new Error(`${propName} value must be defined`)
         }
-        if(propValue < 0){
+        if (propValue < 0) {
             return new Error(`${propName} value must be greater than zero`)
         }
-    } 
+    }
 }
