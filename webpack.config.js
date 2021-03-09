a9cffa7b3fa7a925a009e34a3b0b1b9e56dd5f95
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-08 21:08:36 -0500

    PWA: generating manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
 
 module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+  },
   output: {
-    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].js',
     publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html')
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Your pet photo app',
+      shortname: 'Petgram',
+      description: 'Here you can share photos of your pets and see those of your friends.',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.svg'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ],
   resolve: {
@@ -20,7 +38,18 @@ module.exports = {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
         exclude: /node_modules/,
-      }
+      },
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg|mp4|webm|wav)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000,
+            name: '[contenthash].[ext]',
+            outputPath: 'assets'
+          }
+        },
+      },
     ]
   },
   devServer: {
