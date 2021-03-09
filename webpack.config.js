cb08177aa1c394959120afe9a306795d91d95a49
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-03-08 22:49:42 -0500

    PWA, offline support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -17,14 +18,37 @@ module.exports = {
     }),
     new WebpackPwaManifestPlugin({
       name: 'Petgram - Your pet photo app',
+      name: 'Petgram',
       shortname: 'Petgram',
       description: 'Here you can share photos of your pets and see those of your friends.',
+      orientation: 'portrait',
+      display: 'standalone',
       background_color: '#fff',
       theme_color: '#b1a',
       icons: [
         {
-          src: path.resolve('src/assets/icon.svg'),
-          sizes: [96, 128, 192, 256, 384, 512]
+          src: path.resolve(__dirname, 'src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('Icons'),
+          ios: true,
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          },
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-mateombar.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          },
         }
       ]
     })
