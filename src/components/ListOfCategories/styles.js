d58fe5f341c710af0edfa9d5d7446e7e9be48f95
Tu nombre <tu@ejemplo.cojulian_1997_01@hotmail.com>
2022-02-05 02:07:42 -0300

    "a couple of errors fixed"

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,8 +4,22 @@ export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  &::-webkit-scrollbar {
+  ::-webkit-scrollbar {
     display: none;
+  }
+  &.fixed {
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(0.5);
+    z-index: 1;
   }
 `;
 
