49238217547629615d7bc6af4bbb3dd7b307a594
Tu nombre <tu@ejemplo.cojulian_1997_01@hotmail.com>
2022-02-05 01:47:56 -0300

    Added movement in stories when scrolling down

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -4,8 +4,25 @@ export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  &::-webkit-scrollbar {
-    display: none;
+  ::-webkit-scrollbar {
+    display: contents;
+  }
+  &.fixed {
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(0.5);
+    z-index: 1;
   }
 `;
 
